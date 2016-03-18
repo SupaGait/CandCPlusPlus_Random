ae9a139964f110980badaa5b0d4c4288d0d5c473
SupaGait <gklomphaar@gmail.com>
2016-03-18 11:35:21 +0100

    Course 2 - Advanced C
    - Added Array exercise

---
+++ Course2/Array.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Gerard on 18-3-2016.
+//
+
+#include "Array.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int size;
+    int *a, *ra;
+    printf("array size?");
+    scanf("%d", &size);
+
+    a = createArray(size);
+    ra = createRandomArray(size);
+
+    displayArray(a, size);
+    displayArray(ra, size);
+
+    destroyArray(&a);
+    destroyArray(&ra);
+
+    displayArray(a, size);
+    displayArray(ra, size);
+
+    return 0;
+}
+
+int* createArray(int nElle)
+{
+    int* array = (int*)malloc(nElle * sizeof(int));
+    for(int i=0;i< nElle; i++)
+        array[i] = 0;
+
+    return array;
+}
+int* createRandomArray(int nElle)
+{
+    int* array = (int*)malloc(nElle * sizeof(int));
+
+    for(int i=0;i< nElle; i++)
+        array[i] = rand() % (2*nElle);
+
+    return array;
+}
+void destroyArray(int **a)
+{
+    if(*a) {
+        free(*a);
+        *a = 0;
+    }
+}
+void displayArray(int *a, int nElles)
+{
+    if(a)
+    {
+        if(nElles > MAXDISP)
+        {
+            printf("%d, %d...(%d)...%d, %d", a[0], a[1], nElles, a[nElles-2], a[nElles-1]);
+        }
+        else {
+            for (int i = 0; i < nElles; i++) {
+                printf("%d, ", a[i]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("Empty array \n");
+    }
+}
+
---
+++ Course2/Array.h
@@ -0,0 +1,16 @@
+//
+// Created by Gerard on 18-3-2016.
+//
+
+#ifndef TEST_ARRAY_H
+#define TEST_ARRAY_H
+
+#define MAXDISP 100
+
+int* createArray(int nElle);
+int* createRandomArray( int nElle);
+void destroyArray(int **a);
+void displayArray(int *a, int nElles);
+
+
+#endif //TEST_ARRAY_H
